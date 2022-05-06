d5d842d4ad5e7a8d2285f05d96a79f23d1550da4
JonasEmre <y.emretoktas@gmail.com>
2022-05-06 23:52:58 +0300

    * Added tile class

--- run.py
+++ run.py
@@ -1,3 +1,20 @@
 import pygame
+import sys
+from settings import *
+from tiles import Tile
 
 pygame.init()
+screen = pygame.display.set_mode(screen_size)
+clock = pygame.time.Clock()
+test_tile = pygame.sprite.Group(Tile((100, 100), 200))
+
+if __name__ == '__main__':
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        screen.fill((120, 120, 120))
+        test_tile.draw(screen)
+        pygame.display.update()
+        clock.tick(60)
---
+++ settings.py
@@ -0,0 +1,2 @@
+screen_size = (1280, 720)
+tile_size = 64
